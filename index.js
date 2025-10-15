1853652fd71a850f0ce2fe965562a6687de57b25
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-10-15 22:08:45 +0530

    updated code

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ const schoolRouter = require('./routes/school.routes')
 const app = express()
 const PORT = process.env.PORT || 8080;
 app.use(bodyParser.json())
-
+app.use((err, req, res, next) => {
+    console.error(err.stack); 
+    res.status(500).json({ 
+        success: false,
+        error: 'Internal Server Error',
+        message: 'An unexpected error occurred on the server.'
+    });
+});
 app.use('/api',schoolRouter)
 app.listen(PORT,()=>{
     console.log(`Server is Running in PORT: http://localhost:${PORT}`)
